0d0fe55bdacbb9e6c9783ca3fec1bb757b96235e
Szymon Szymański <szymanskiszy@gmail.com>
2018-03-25 20:12:15 +0200

    changed structure to blueprints

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,8 +1,12 @@
 import unittest
-from flask.cli import FlaskGroup
-from project import app, db
 
-cli = FlaskGroup(app)
+from flask.cli import FlaskGroup
+
+from project import create_app, db
+from project.api.models import User
+
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
 
 @cli.command()
 def recreate_db():
