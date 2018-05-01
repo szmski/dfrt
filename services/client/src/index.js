43c65a3d5d01e94f0e8131fcd494b600fca947f8
Szymon Szymański <szymanskiszy@gmail.com>
2018-05-01 12:09:24 +0200

    added Users communication to api via axios, fixed CORS

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -2,7 +2,26 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-class App extends Component {    
+import axios from "axios";
+import UsersList from './components/UsersList';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            users: []
+        }
+    }
+    getUsers() {
+        axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+        .then(res => {this.setState({users: res.data.data.users})})
+        .catch(err => {console.log(err)})
+    }
+
+    componentDidMount() {
+        this.getUsers();        
+    }
+
     render() {
         return (
             <div className="container">
@@ -10,6 +29,7 @@ class App extends Component {
                     <div className="col-md-4">
                         <br/>
                         <h1>All Users</h1>
+                        <UsersList users={this.state.users}/>
                         <hr/><br/>
                     </div>
                 </div>
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -4,6 +4,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS
 
 
 # instantiate the db
@@ -15,6 +16,8 @@ def create_app(script_info=None):
 
     # instantiate the app
     app = Flask(__name__)
+
+    CORS(app)
 
     # set config
     app_settings = os.getenv("APP_SETTINGS")
