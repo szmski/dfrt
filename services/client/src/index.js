90cbfe37c8c8911af3f36cdc2ad2d01a078fd3e9
Szymon Szymański <szymanskiszy@gmail.com>
2018-05-03 15:05:16 +0200

    added router and example route

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,77 +1,12 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-import axios from "axios";
-import UsersList from './components/UsersList';
-import AddUser from './components/AddUser';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            users: [],
-            username: '',
-            email: ''
-        }
+import App from './App.jsx';
 
-        this.addUser = this.addUser.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
-    getUsers() {
-        axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
-        .then(res => {this.setState({users: res.data.data.users})})
-        .catch(err => {console.log(err)})
-    }
-
-    addUser(event) {
-        event.preventDefault();
-
-        const data = {
-            username: this.state.username,
-            email: this.state.email
-        }
-
-        axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
-        .then((res) => {
-            this.getUsers();
-            this.setState({
-                username: '', 
-                email: ''
-            })
-        })
-        .catch((err) => {console.log(err)})
-    }
-
-    handleChange(event) {
-        const obj = {};
-        obj[event.target.name] = event.target.value;
-        this.setState(obj);
-    }
-
-    componentDidMount() {
-        this.getUsers();        
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-4">
-                        <h1>All Users</h1>
-                        <br/>
-                        <AddUser
-                            username={this.state.username}
-                            email={this.state.email}
-                            addUser={this.addUser}
-                            handleChange={this.handleChange}/>
-                        <hr/><br/>
-                        <UsersList users={this.state.users}/>
-                    </div>
-                </div>
-            </div>
-            )
-        }
-};
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render((
+    <Router>
+        <App/>
+    </Router>
+),document.getElementById('root'))
